71b63bf60291fd92bec92fa63304e6d4419d5f25
Wellington Marthas <well@dyntime.com>
2020-06-27 00:35:15 -0300

    Small changes

--- Package.swift
+++ Package.swift
@@ -47,9 +47,12 @@ let package = Package(
                 
                 .define("SQLITE_DEFAULT_MEMSTATUS", to: nil),
                 .define("SQLITE_DEFAULT_WAL_SYNCHRONOUS", to: "1"),
+                .define("SQLITE_DQS", to: "0"),
                 .define("SQLITE_LIKE_DOESNT_MATCH_BLOBS", to: nil),
                 .define("SQLITE_MAX_EXPR_DEPTH", to: "0"),
                 .define("SQLITE_MAX_MMAP_SIZE", to: "0"),
+                .define("SQLITE_OMIT_AUTOINIT", to: nil),
+                .define("SQLITE_OMIT_DECLTYPE", to: nil),
                 .define("SQLITE_OMIT_DEPRECATED", to: nil),
                 .define("SQLITE_OMIT_LOAD_EXTENSION", to: nil),
                 .define("SQLITE_OMIT_PROGRESS_CALLBACK", to: nil),
