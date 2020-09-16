49d81f97885246dcab99833f7c8fc378286c47dd
Wellington Marthas <well@dyntime.com>
2020-09-16 02:36:30 -0300

    Small changes

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ import PackageDescription
 let package = Package(
     name: "SQLCipher",
     platforms: [
-        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
+        .macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)
     ],
     products: [
         .library(
